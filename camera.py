7e72424394b3ba464aff80be46b67f602c281fbc
Scott <eiselesr@gmail.com>
2017-05-09 12:22:20 -0500

    take video

--- camera.py
+++ camera.py
@@ -8,30 +8,18 @@ args = parser.parse_args()
 
 print(args.fileName)
 
-with PiCamera() as camera:
-    #camera.resolution = (2592, 1944) #max resolution for still images
-    sleep(5)
-    #camera.capture('/home/pi/Desktop/%s.jpg' % fname)
-    camera.capture('/home/pi/Desktop/%s.jpg' % args.fileName)
+##with PiCamera() as camera:
+##    #camera.resolution = (2592, 1944) #max resolution for still images
+##    camera.start_preview()
+##    sleep(5)
+##    camera.capture('/home/pi/Desktop/%s.jpg' % args.fileName)
+##    camera.stop_preview()
 
-### camera.resolution = (64, 64)
-##camera.framerate = 15 # framerate must be set to 15 to permit high resolution images. 
-##
-###camera.start_preview(alpha=255) #turn camera on.
-##camera.annotate_text = "Hello World!"
-##
-###take five pictures
-###for i in range(5):
-##i = 1
-##sleep(5) #must sleep at least 2 seconds before capturing so sensor can set light levels
-##camera.capture('/home/pi/Desktop/%s.jpg' % fname)
-##
-### Record video
-###camera.start_recording('/home/pi/Desktop/video.h264')
-###sleep(10)
-###camera.stop_recording()
-##
-##
-##    
-###camera.stop_preview()
+with PiCamera() as camera:
+    camera.start_preview()
+    camera.start_recording('/home/pi/Desktop/%s.h264' % args.fileName)
+    sleep(20)
+    camera.stop_recording()
+    camera.stop_preview()
+
 
